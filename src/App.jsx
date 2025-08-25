be84e653c1f6a8f7cfacab1b7e4b00abcca86495
Praeda822 <patdkelly5@gmail.com>
2025-08-25 12:10:06 +1000

    added persistence to localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import NoteForm from "./assets/components/NoteForm";
 import NoteList from "./assets/components/NoteList";
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(() => {
+    // Stores notes in local storage so they persist on refresh
+    // JSON.parse converts the string back into an array
+    const notes = JSON.parse(localStorage.getItem("notes"));
+    // If there are no notes, it returns an empty (default) array
+    return notes || [];
+  });
 
   // Will run when anything in the depenency array changes
   // Since notes is in the dependency array, this will run whenever a note changes
